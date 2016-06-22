08beacebcf0e155085c52307a4e7bdc959290fd5
Steffkn <steff.kn@abv.bg>
2016-06-22 12:18:46 +0300

    Add: Simple DocX generation project Add: ImageDownloader

---
+++ CSharp/DocXManipulator/DocXManipulator/DocXManipulator/Program.cs
@@ -0,0 +1,41 @@
+﻿using Novacode;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DocXManipulator
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Modify to suit your machine:
+            string fileName = @"F:\DocXExample.docx";
+
+            Dictionary<string, string> propartiesDict = new Dictionary<string, string>();
+            propartiesDict.Add("[USER_NAME]", "Стефан Николов Петров");
+            propartiesDict.Add("[USER_FNUMBER]", "111210045");
+            propartiesDict.Add("[USER_ADDRESS]", "гр. Кюстендил, кв. 'Запад', бл. 71, ап.21");
+            propartiesDict.Add("[USER_TEL]", "0883332383");
+            propartiesDict.Add("[USER_EMAIL]", "steff.kn@abv.bg");
+            propartiesDict.Add("[DOC_TITLE]", "Very long title of diploma for making a system that makes diplomas. Amazing");
+            propartiesDict.Add("[DOC_TECH]", "list of dechs");
+            propartiesDict.Add("[DOC_BEGINDATE]", "01/2016");
+            propartiesDict.Add("[DOC_ENDDATA]", "01/2017");
+
+            // Create a document in memory:
+            var doc = DocX.Create(fileName);
+            doc.ApplyTemplate(@"F:\sp-temp.docx");
+
+            foreach (var prop in propartiesDict)
+            {
+                doc.ReplaceText(prop.Key, prop.Value);
+            }
+
+            // Save to the output directory:
+            doc.Save();
+
+            // Open in Word:
+            Process.Start("WINWORD.EXE", fileName);
+        }
+    }
+}
---
+++ CSharp/ImageDownloader/ImageDownloader/ImageDownloader/Program.cs
@@ -0,0 +1,79 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ImageDownloader
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var configuration = Configuration.Default.WithDefaultLoader();
+            var browsingContext = BrowsingContext.New(configuration);
+            var location = @"f:\temp";
+            var repo = $"http://www...";
+
+            IList<string> galleries = new List<string>();
+
+            var site = @"http://www...";
+            var documentSite = browsingContext.OpenAsync(site).Result;
+            var aTags = documentSite.QuerySelectorAll("#gallery-list a");
+
+            foreach (var item in aTags)
+            {
+                foreach (var attr in item.Attributes)
+                {
+                    if (attr.Name == "href")
+                    {
+                        var names = attr.Value.Split('/');
+                        galleries.Add(names[1]);
+                        break;
+                    }
+                }
+            }
+
+            for (int g = 0; g < galleries.Count; g++)
+            {
+                var gallery = galleries[g];
+                Console.WriteLine("{0} [{1}/{2}]", gallery, g, galleries.Count);
+                var file = gallery.Substring(0, gallery.IndexOf('-'));
+
+                Directory.CreateDirectory(string.Format(@"{0}\{1}\{2}", location, file, gallery));
+
+                for (int i = 1; i <= 20; i++)
+                {
+                    var url = string.Format("{0}{1}/{2}-{3}", repo, gallery, file, i);
+                    var document = browsingContext.OpenAsync(url).Result;
+
+                    try
+                    {
+                        var imageTag = document.QuerySelector(".theCell img").Attributes;
+
+                        foreach (var item in imageTag)
+                        {
+                            if (item.Name == "src")
+                            {
+                                Console.WriteLine("Downloading.. {0}", item.Value);
+                                using (WebClient client = new WebClient())
+                                {
+                                    var imgDir = string.Format(@"{0}\{1}\{2}\{3}", location, file, gallery, item.Value.Substring(item.Value.LastIndexOf('/') + 1));
+                                    client.DownloadFile(new Uri(item.Value), imgDir);
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
